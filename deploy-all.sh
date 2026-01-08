00f65a015c574164268d61918845153fa27d5e85
Gaelle Fournier <gaelle.fournier.work@gmail.com>
2026-01-08 10:11:19 +0100

    comment cronjob

--- deploy-all.sh
+++ deploy-all.sh
@@ -9,8 +9,8 @@ oc new-project camel-demo-1
 oc new-project camel-demo-2
 oc project default
 
-podman login --tls-verify=false -u kubeadmin -p $(oc whoami -t) default-route-openshift-image-registry.apps-crc.testing
-docker login -u kubeadmin -p $(oc whoami -t) default-route-openshift-image-registry.apps-crc.testing
+#podman login --tls-verify=false -u kubeadmin -p $(oc whoami -t) default-route-openshift-image-registry.apps-crc.testing
+#docker login -u kubeadmin -p $(oc whoami -t) default-route-openshift-image-registry.apps-crc.testing
 
 echo 'Deploy camel-main'
 oc project camel-demo-1
@@ -37,9 +37,9 @@ pushd $location/camel-spring-jolokia
 ./mvnw clean package oc:deploy
 popd
 
-echo 'Deploy camel-cronjob'
-oc project camel-demo-2
-pushd $location/camel-cronjob
-./mvnw clean package -Dquarkus.openshift.deploy=true
-popd
+#echo 'Deploy camel-cronjob'
+#oc project camel-demo-2
+#pushd $location/camel-cronjob
+#./mvnw clean package -Dquarkus.openshift.deploy=true
+#popd
 
